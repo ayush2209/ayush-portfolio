3699acc12f944ba05bab58a82cee7e1180c7a216
ayush2209 <53752682+ayush2209@users.noreply.github.com>
2023-01-19 20:51:55 +0530

    added font awesome for social media icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/Startup/navbar/navbar.component';
 import { HomeComponent } from './Component/Startup/home/home.component';
