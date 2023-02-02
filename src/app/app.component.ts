ba5a5f03e1e623bea3d7712d2162c9736f1334fc
ayush2209 <53752682+ayush2209@users.noreply.github.com>
2023-02-02 11:12:04 +0530

    color , remove logger

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,7 @@ export class AppComponent {
   @HostListener('window:scroll')
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    console.log('[scroll]', scrollPosition);
+    // console.log('[scroll]', scrollPosition);
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
     } else {
