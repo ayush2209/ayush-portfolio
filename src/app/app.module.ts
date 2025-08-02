75154ad838763ea2a302fcc6dd6d5f7c81e6b08f
ayush2209 <53752682+ayush2209@users.noreply.github.com>
2025-08-02 16:21:40 +0530

    Angular Version Upgraded to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { NgOptimizedImage } from '@angular/common'
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Component/Startup/navbar/navbar.component';
@@ -20,54 +20,52 @@ import { FormsModule } from '@angular/forms';
 import { HttpInterceptorService } from './Shared/Service/http-interceptor.service';
 import { UpperCasePipe } from './upper-case.pipe';
 import { TextHightlightDirective } from './text-hightlight.directive';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    AboutAyushComponent,
-    CareerComponent,
-    ProjectComponent,
-    TechnologyComponent,
-    LoaderComponent,
-    CommonModalComponent,
-    UpperCasePipe,
-    TextHightlightDirective
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    NgxSpinnerModule,
-    NgOptimizedImage,
-    FormsModule,
-    HttpClientModule,
-    NgOptimizedImage,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-    BsDropdownModule
-  ],
-  providers: [
-    BsModalService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: HttpInterceptorService,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        AboutAyushComponent,
+        CareerComponent,
+        ProjectComponent,
+        TechnologyComponent,
+        LoaderComponent,
+        CommonModalComponent,
+        UpperCasePipe,
+        TextHightlightDirective
+    ],
+    bootstrap: [AppComponent], imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        NgxSpinnerModule,
+        NgOptimizedImage,
+        FormsModule,
+        TranslateModule.forRoot({
+            defaultLanguage: 'en',
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
+        BsDropdownModule
+    ], providers: [
+        BsModalService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: HttpInterceptorService,
+            multi: true
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ]
 })
 export class AppModule { }
